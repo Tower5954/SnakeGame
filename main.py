5d6c5223fa0e8d9fc974c3c6cb88df26b78a5f9b
Tower5954 <rvanna@hotmail.co.uk>
2020-12-20 05:52:26 +0000

    Snake class added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
 import time
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("Tower's Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -16,21 +20,34 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-game_is_on =True
+game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
-
+    time.sleep(0.1)
     snake.move()
 
+    #Detect eating food
+    if snake.head.distance(food) < 18:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    #Detect biting body
+    # for segment in snake.segments:
+    #     if segment == snake.head:
+    #         pass
+    #     elif snake.head.distance(segment) < 10:
+    #         game_is_on = False
+    #         scoreboard.game_over()
+    for segments in snake.segments[1:]:
+        if snake.head.distance(segments) < 5:
+            game_is_on = False
+            scoreboard.game_over()
 
-
-
-
-
-
-
+    #Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 screen.exitonclick()
